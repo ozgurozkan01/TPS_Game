03742c2ca86dcc29cca8a8ebc346477c1bdda186
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2023-11-29 13:17:49 +0300

    add enhanced input system module

--- Source/TPS_Game/TPS_Game.Build.cs
+++ Source/TPS_Game/TPS_Game.Build.cs
@@ -8,7 +8,7 @@ public class TPS_Game : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput"});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
