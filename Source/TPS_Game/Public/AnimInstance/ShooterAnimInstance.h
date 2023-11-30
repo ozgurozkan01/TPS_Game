7e570a2f3708625fcb6407eabe1b82c00eb42615
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2023-11-30 11:33:04 +0300

    add shooter anim instance class to control animations

---
+++ Source/TPS_Game/Public/AnimInstance/ShooterAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "ShooterAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TPS_GAME_API UShooterAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
