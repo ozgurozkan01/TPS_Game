d9e4db38cfe08fb45753ffa13afda2ee9e303f69
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2023-11-30 18:20:35 +0300

    add variables to control the animation flow with C++

--- Source/TPS_Game/Private/AnimInstance/ShooterAnimInstance.cpp
+++ Source/TPS_Game/Private/AnimInstance/ShooterAnimInstance.cpp
@@ -2,4 +2,44 @@
 
 
 #include "AnimInstance/ShooterAnimInstance.h"
+#include "Character/ShooterCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
+void UShooterAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	ShooterCharacter = Cast<AShooterCharacter>(TryGetPawnOwner());
+}
+
+void UShooterAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (ShooterCharacter == nullptr)
+	{
+		ShooterCharacter = Cast<AShooterCharacter>(TryGetPawnOwner());
+	}
+
+	if (ShooterCharacter)
+	{
+		/** Set the speed of character */
+		FVector CurrentVelocity = ShooterCharacter->GetCharacterMovement()->Velocity;
+		CurrentVelocity.Z = 0.f;
+		Speed = CurrentVelocity.Size();
+
+		/** Whether or not the character is in air?*/
+		bIsInAir = ShooterCharacter->GetCharacterMovement()->IsFalling();
+		
+		/** Whether or not the character is accelerating?*/
+		if (ShooterCharacter->GetCharacterMovement()->GetCurrentAcceleration().Size() > 0.f)
+		{
+			bIsAccelerating = true;
+		}
+
+		else
+		{
+			bIsAccelerating = false;
+		}
+	}
+}
--- Source/TPS_Game/Public/AnimInstance/ShooterAnimInstance.h
+++ Source/TPS_Game/Public/AnimInstance/ShooterAnimInstance.h
@@ -6,6 +6,7 @@
 #include "Animation/AnimInstance.h"
 #include "ShooterAnimInstance.generated.h"
 
+class AShooterCharacter;
 /**
  * 
  */
@@ -14,4 +15,20 @@ class TPS_GAME_API UShooterAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
 	
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Movement, meta=(AllowPrivateAccess = "true"))
+	TObjectPtr<AShooterCharacter> ShooterCharacter;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Movement, meta=(AllowPrivateAccess = "true"))
+	float Speed;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Movement, meta=(AllowPrivateAccess = "true"))
+	bool bIsInAir;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Movement, meta=(AllowPrivateAccess = "true"))
+	bool bIsAccelerating;
 };
