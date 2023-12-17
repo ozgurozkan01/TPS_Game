d8f1a95a5ea70864532fb8739a45889c92ebd1f5
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2023-12-17 11:47:00 +0300

    add empty weapon file which is derived from the base item class

---
+++ Source/TPS_Game/Public/Item/Weapon.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Item/BaseItem.h"
+#include "Weapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TPS_GAME_API AWeapon : public ABaseItem
+{
+	GENERATED_BODY()
+	
+};
