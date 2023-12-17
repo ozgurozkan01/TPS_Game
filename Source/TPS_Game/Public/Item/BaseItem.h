43ee3b0173a51c5a0d3e21dfd08177b8e633c027
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2023-12-17 11:17:15 +0300

    add empty base item class to derive specific items

---
+++ Source/TPS_Game/Public/Item/BaseItem.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BaseItem.generated.h"
+
+UCLASS()
+class TPS_GAME_API ABaseItem : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABaseItem();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
