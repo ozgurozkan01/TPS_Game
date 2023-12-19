36df2bf23bdf938da582128390efeafef24518f7
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2023-12-20 00:45:29 +0300

    add item information pop up widget class

---
+++ Source/TPS_Game/Public/HUD/InformationPopUp.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "InformationPopUp.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TPS_GAME_API UInformationPopUp : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
