d4deb4e0d203b571739b52b38c07522fc3af48e3
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2023-12-21 19:40:41 +0300

    add variables which is changable accordingly what is the item and their setters

--- Source/TPS_Game/Private/HUD/InformationPopUp.cpp
+++ Source/TPS_Game/Private/HUD/InformationPopUp.cpp
@@ -3,3 +3,50 @@
 
 #include "HUD/InformationPopUp.h"
 
+#include "Components/Image.h"
+
+void UInformationPopUp::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+
+	if (Star1Image && Star2Image && Star3Image && Star4Image && Star5Image)
+	{
+		StarImageArray.Add(Star1Image);
+		StarImageArray.Add(Star2Image);
+		StarImageArray.Add(Star3Image);
+		StarImageArray.Add(Star4Image);
+		StarImageArray.Add(Star5Image);
+	}
+}
+
+void UInformationPopUp::SetItemNameText(FString& ItemName)
+{
+	ItemNameText->SetText(FText::FromString(ItemName));
+}
+
+void UInformationPopUp::SetAmmoAmountText(int8 Amount)
+{
+	FString AmmoAmountFromInt = FString::FromInt(Amount);
+	AmmoAmountText->SetText(FText::FromString(AmmoAmountFromInt));
+}
+
+void UInformationPopUp::SetStarsImagesVisibility(TArray<bool> ActiveStarsArray)
+{
+	if (StarImageArray.Num() == 0) { return; }
+	
+	for (uint8 i = 0; i < 5; i++)
+	{
+		if (StarImageArray[i])
+		{
+			if (ActiveStarsArray[i] == true)
+			{
+				StarImageArray[i]->SetOpacity(1);
+			}
+
+			else
+			{
+				StarImageArray[i]->SetOpacity(0);
+			}	
+		}
+	}
+}
--- Source/TPS_Game/Public/HUD/InformationPopUp.h
+++ Source/TPS_Game/Public/HUD/InformationPopUp.h
@@ -3,15 +3,42 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "HLSLTypeAliases.h"
 #include "Blueprint/UserWidget.h"
+#include "Components/TextBlock.h"
 #include "InformationPopUp.generated.h"
 
-/**
- * 
- */
+class UImage;
+class UTextBlock;
+class ABaseItem;
+
 UCLASS()
 class TPS_GAME_API UInformationPopUp : public UUserWidget
 {
 	GENERATED_BODY()
+public:
+	virtual void NativeOnInitialized() override;
 	
+	// Getters and Setters
+	void SetItemNameText(FString& ItemName);
+	void SetAmmoAmountText(int8 Amount);
+	void SetStarsImagesVisibility(TArray<bool> ActiveStarsArray);
+private:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Item, meta=(AllowPrivateAccess = "true"), meta=(BindWidget))
+	TObjectPtr<UTextBlock> ItemNameText;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Item, meta=(AllowPrivateAccess = "true"), meta=(BindWidget))
+	TObjectPtr<UTextBlock> AmmoAmountText;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Item, meta=(AllowPrivateAccess = "true"), meta=(BindWidget))
+	TObjectPtr<UImage> Star1Image;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Item, meta=(AllowPrivateAccess = "true"), meta=(BindWidget))
+	TObjectPtr<UImage> Star2Image;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Item, meta=(AllowPrivateAccess = "true"), meta=(BindWidget))
+	TObjectPtr<UImage> Star3Image;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Item, meta=(AllowPrivateAccess = "true"), meta=(BindWidget))
+	TObjectPtr<UImage> Star4Image;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Item, meta=(AllowPrivateAccess = "true"), meta=(BindWidget))
+	TObjectPtr<UImage> Star5Image;
+
+	TArray<TObjectPtr<UImage>> StarImageArray;
 };
