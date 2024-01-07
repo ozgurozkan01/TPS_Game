97608c72232ebb60106bdb2c5d94804021199f9c
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-01-07 16:01:11 +0300

    add update current ammo text function and impelement it

--- Source/TPS_Game/Private/HUD/AmmoWidget.cpp
+++ Source/TPS_Game/Private/HUD/AmmoWidget.cpp
@@ -3,3 +3,36 @@
 
 #include "HUD/AmmoWidget.h"
 
+#include "Character/ShooterCharacter.h"
+#include "Components/TextBlock.h"
+#include "Item/Weapon.h"
+#include "Kismet/GameplayStatics.h"
+
+void UAmmoWidget::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+
+	TObjectPtr<APawn> Pawn = UGameplayStatics::GetPlayerPawn(this, 0);
+
+	if (Pawn)
+	{
+		ShooterRef = Cast<AShooterCharacter>(Pawn);
+	}
+
+	UpdateCurrentAmmoText();
+}
+
+FText UAmmoWidget::UpdateCurrentAmmoText()
+{
+	if (ShooterRef == nullptr || ShooterRef->GetEquippedWeapon() == nullptr) { return FText(); }
+
+	FString CurrentAmmoInt = FString::FromInt(ShooterRef->GetEquippedWeapon()->GetCurrentAmmo());
+	FText CurrentAmmo = FText::FromString(CurrentAmmoInt);
+
+	return CurrentAmmo;
+}
+
+void UAmmoWidget::UpdateMaxAmmoText()
+{
+
+}
