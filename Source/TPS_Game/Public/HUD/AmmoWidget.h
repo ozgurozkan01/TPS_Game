2b1582a7f4008557176aacbc43fd88f8e3272041
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-01-07 16:01:52 +0300

    add widget variables to bind and functions to update variables values

--- Source/TPS_Game/Public/HUD/AmmoWidget.h
+++ Source/TPS_Game/Public/HUD/AmmoWidget.h
@@ -6,12 +6,27 @@
 #include "Blueprint/UserWidget.h"
 #include "AmmoWidget.generated.h"
 
-/**
- * 
- */
+class UTextBlock;
+class AShooterCharacter;
+
 UCLASS()
 class TPS_GAME_API UAmmoWidget : public UUserWidget
 {
 	GENERATED_BODY()
+
+protected:
+	virtual void NativeOnInitialized() override;
+public:
+	UFUNCTION(BlueprintCallable)
+	FText UpdateCurrentAmmoText();
+	UFUNCTION(BlueprintCallable)
+	void UpdateMaxAmmoText();
+private:
+
+	TObjectPtr<AShooterCharacter> ShooterRef;
 	
+	UPROPERTY(meta=(BindWidget), meta=(AllowPrivateAccess = "true"))
+	TObjectPtr<UTextBlock> CurrentAmmoText;
+	UPROPERTY(meta=(BindWidget), meta=(AllowPrivateAccess = "true"))
+	TObjectPtr<UTextBlock> StoredAmmoText;
 };
