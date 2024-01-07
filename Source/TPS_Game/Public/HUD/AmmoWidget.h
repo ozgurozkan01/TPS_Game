b7aba7619428626a6f51dab72e66e759f376053a
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-01-07 13:16:38 +0300

    add ammo widget class to control the widget text properties

---
+++ Source/TPS_Game/Public/HUD/AmmoWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "AmmoWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TPS_GAME_API UAmmoWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
