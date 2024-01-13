dbe4caf972b4b1564911d2a5189cba90339535f6
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-01-13 23:56:01 +0300

    create weapon anim instance to use as parent in the ABP

---
+++ Source/TPS_Game/Public/AnimInstance/WeaponAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "WeaponAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TPS_GAME_API UWeaponAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
