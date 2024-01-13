5f048a6f0640bb68577ec7b54aa47b4a8058ffb0
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-01-13 13:37:05 +0300

    add ammo type enum class

--- Source/TPS_Game/Public/AmmoType.h
+++ Source/TPS_Game/Public/AmmoType.h
@@ -1,2 +1,10 @@
 ﻿#pragma once
 
+UENUM(BlueprintType)
+enum class EAmmoType : uint8
+{
+	EAT_9mm UMETA(DisplayName = "9mm"),
+	EAT_AR UMETA(DisplayName = "Assault Rifle"),
+
+	EAT_MAX UMETA(DisplayName = "Default")
+};
