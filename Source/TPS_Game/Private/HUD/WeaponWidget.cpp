03660795a81d633aebb2f2e5c0b499ee4b2d512f
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-01-13 17:02:40 +0300

    rename the ammoWidget class as weaponWidget

---
+++ Source/TPS_Game/Private/HUD/WeaponWidget.cpp
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HUD/WeaponWidget.h"
+#include "Character/ShooterCharacter.h"
+#include "Item/Weapon.h"
+#include "Kismet/GameplayStatics.h"
+
+void UWeaponWidget::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+
+	TObjectPtr<APawn> Pawn = UGameplayStatics::GetPlayerPawn(this, 0);
+
+	if (Pawn)
+	{
+		ShooterRef = Cast<AShooterCharacter>(Pawn);
+	}
+
+	UpdateCurrentAmmoText();
+}
+
+FText UWeaponWidget::UpdateCurrentAmmoText()
+{
+	if (ShooterRef == nullptr || ShooterRef->GetEquippedWeapon() == nullptr) { return FText(); }
+
+	FString CurrentAmmoString = FString::FromInt(ShooterRef->GetEquippedWeapon()->GetCurrentAmmo());
+	FText CurrentAmmoText = FText::FromString(CurrentAmmoString);
+
+	return CurrentAmmoText;
+}
+
+FText UWeaponWidget::UpdateMaxAmmoText()
+{
+	if (ShooterRef == nullptr || ShooterRef->GetEquippedWeapon() == nullptr) { return FText(); }
+
+	FString CurrentMaxAmmoString = FString::FromInt(ShooterRef->GetAmmoCountByWeaponType());
+	FText CurrentMaxAmmoText = FText::FromString(CurrentMaxAmmoString);
+
+	return CurrentMaxAmmoText;
+}
+
+FText UWeaponWidget::UpdateWeaponNameText()
+{
+	if (ShooterRef == nullptr || ShooterRef->GetEquippedWeapon() == nullptr) { return FText(); }
+
+	FString CurrentMaxAmmoString = ShooterRef->GetWeaponName();
+	FText CurrentMaxAmmoText = FText::FromString(CurrentMaxAmmoString);
+
+	return CurrentMaxAmmoText;
+
+}
