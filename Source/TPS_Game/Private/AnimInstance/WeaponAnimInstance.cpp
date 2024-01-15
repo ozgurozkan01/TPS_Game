0b89bbe9788ec7d222b262fef7b27db92df1d015
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-01-15 23:20:06 +0300

    add weapon anim instance to control weapon animation system

--- Source/TPS_Game/Private/AnimInstance/WeaponAnimInstance.cpp
+++ Source/TPS_Game/Private/AnimInstance/WeaponAnimInstance.cpp
@@ -3,3 +3,30 @@
 
 #include "AnimInstance/WeaponAnimInstance.h"
 
+#include "Character/ShooterCharacter.h"
+#include "Kismet/GameplayStatics.h"
+
+void UWeaponAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	if (ShooterRef == nullptr)
+	{
+		ShooterRef = Cast<AShooterCharacter>(UGameplayStatics::GetPlayerPawn(this, 0));
+	}
+}
+
+void UWeaponAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+	
+	if (ShooterRef == nullptr)
+	{
+		ShooterRef = Cast<AShooterCharacter>(UGameplayStatics::GetPlayerPawn(this, 0));
+	}
+
+	if (ShooterRef)
+	{
+		LeftHandSceneCompTransform = ShooterRef->GetLeftHandSceneComp()->GetComponentTransform();
+	}
+}
--- Source/TPS_Game/Public/AnimInstance/WeaponAnimInstance.h
+++ Source/TPS_Game/Public/AnimInstance/WeaponAnimInstance.h
@@ -6,12 +6,25 @@
 #include "Animation/AnimInstance.h"
 #include "WeaponAnimInstance.generated.h"
 
-/**
- * 
- */
+class AShooterCharacter;
+class AWeapon;
+class USceneComponent;
+
 UCLASS()
 class TPS_GAME_API UWeaponAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
-	
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+private:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Animation, meta=(AllowPrivateAccess="true"))
+	TObjectPtr<AShooterCharacter> ShooterRef;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Animation, meta=(AllowPrivateAccess="true"))
+	TObjectPtr<AWeapon> OwningWeaponRef;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Animation, meta=(AllowPrivateAccess="true"))
+	FTransform LeftHandSceneCompTransform;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Animation, meta=(AllowPrivateAccess="true"))
+	bool bIsMovingMagazine;
 };
