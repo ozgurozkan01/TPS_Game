0e3754de18a8fd3eedd3dbec103e8b6c863d806a
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-01-25 12:26:12 +0300

    add crosshair animator component to control crosshair behavior accordingly character movement

--- Source/TPS_Game/Private/Components/CrosshairAnimatorComponent.cpp
+++ Source/TPS_Game/Private/Components/CrosshairAnimatorComponent.cpp
@@ -3,32 +3,103 @@
 
 #include "Components/CrosshairAnimatorComponent.h"
 
-// Sets default values for this component's properties
-UCrosshairAnimatorComponent::UCrosshairAnimatorComponent()
-{
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+#include "Character/ShooterCharacter.h"
+#include "Components/TracerComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
-	// ...
+UCrosshairAnimatorComponent::UCrosshairAnimatorComponent() :
+	CrosshairSpreadMultiplier(0.5f),
+	CrosshairInAirMultiplier(0.f),
+	CrosshairAimingMultiplier(0.f),
+	CrosshairShootingMultiplier(0.f),
+	CrosshairSpreadMax(16.f)
+{
+	PrimaryComponentTick.bCanEverTick = true;
 }
 
-
-// Called when the game starts
 void UCrosshairAnimatorComponent::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
-	
+	if (MainCharacter == nullptr)
+	{
+		MainCharacter = Cast<AShooterCharacter>(GetOwner());
+	}
 }
 
-
-// Called every frame
 void UCrosshairAnimatorComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	CalculateCrosshairSpreadMultiplier(DeltaTime);
 }
 
+void UCrosshairAnimatorComponent::CalculateCrosshairSpreadMultiplier(float DeltaTime)
+{
+	CrosshairSpreadMultiplier =
+		0.5 +
+		CalculateCrosshairVelocityMultiplier() +
+		CalculateCrosshairInAirMultiplier(DeltaTime) +
+		CalculateCrosshairFireAimingMultiplier(DeltaTime) -
+		CalculateCrosshairAimingMultiplier(DeltaTime);
+}
+
+float UCrosshairAnimatorComponent::CalculateCrosshairVelocityMultiplier()
+{
+	if (MainCharacter)
+	{
+		FVector2D WalkSpeedRange {0.f, 600.f};
+		FVector2D VelocityMultiplierRange {0.f, 1.f};
+		FVector CurrentVelocity = MainCharacter->GetVelocity();
+		CurrentVelocity.Z = 0.f;
+
+		/** Clamp the speed value based on output range value and return corresponding percentage accordingly output*/
+		return FMath::GetMappedRangeValueClamped(WalkSpeedRange, VelocityMultiplierRange, CurrentVelocity.Size());	
+	}
+	return 0.f;
+}
+
+float UCrosshairAnimatorComponent::CalculateCrosshairInAirMultiplier(float DeltaTime)
+{
+	if (MainCharacter && MainCharacter->GetCharacterMovement()->IsFalling())
+	{
+		CrosshairInAirMultiplier = FMath::FInterpTo(CrosshairInAirMultiplier, 2.25f, DeltaTime, 6.f); 
+	}
+
+	else
+	{
+		CrosshairInAirMultiplier = FMath::FInterpTo(CrosshairInAirMultiplier, 0, DeltaTime, 10.f); 
+	}
+
+	return CrosshairInAirMultiplier;
+}
+
+float UCrosshairAnimatorComponent::CalculateCrosshairAimingMultiplier(float DeltaTime)
+{
+	if (MainCharacter && MainCharacter->GetIsAiming())
+	{
+		CrosshairAimingMultiplier = FMath::FInterpTo(CrosshairAimingMultiplier, 0.35f, DeltaTime, 6.f); 
+	}
+
+	else
+	{
+		CrosshairAimingMultiplier = FMath::FInterpTo(CrosshairAimingMultiplier, 0, DeltaTime, 10.f); 
+	}
+
+	return CrosshairAimingMultiplier;
+	
+}
+
+float UCrosshairAnimatorComponent::CalculateCrosshairFireAimingMultiplier(float DeltaTime)
+{
+	if (MainCharacter && MainCharacter->GetTracerComponent() && MainCharacter->GetTracerComponent()->GetIsFiring())
+	{
+		CrosshairShootingMultiplier = FMath::FInterpTo(CrosshairShootingMultiplier, 0.2f, DeltaTime, 45.f); 
+	}
+
+	else
+	{
+		CrosshairShootingMultiplier = FMath::FInterpTo(CrosshairShootingMultiplier, 0.f, DeltaTime, 45.f); 
+	}
+
+	return CrosshairShootingMultiplier;
+}
--- Source/TPS_Game/Public/Components/CrosshairAnimatorComponent.h
+++ Source/TPS_Game/Public/Components/CrosshairAnimatorComponent.h
@@ -7,6 +7,8 @@
 #include "CrosshairAnimatorComponent.generated.h"
 
 
+class AShooterCharacter;
+
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class TPS_GAME_API UCrosshairAnimatorComponent : public UActorComponent
 {
@@ -24,5 +26,30 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+private:
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Crosshair, meta=(AllowPrivateAccess="true"))
+	float CrosshairSpreadMultiplier;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Crosshair, meta=(AllowPrivateAccess="true"))
+	float CrosshairInAirMultiplier;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Crosshair, meta=(AllowPrivateAccess="true"))
+	float CrosshairAimingMultiplier;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Crosshair, meta=(AllowPrivateAccess="true"))
+	float CrosshairShootingMultiplier;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Crosshair, meta=(AllowPrivateAccess="true"))
+	float CrosshairSpreadMax;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Owner, meta=(AllowPrivateAccess="true"))
+	TObjectPtr<AShooterCharacter> MainCharacter;
+public:
+	
+	/** Functions that calculate values that affect the crosshair's spread animation */
+	void CalculateCrosshairSpreadMultiplier(float DeltaTime);
+	float CalculateCrosshairVelocityMultiplier();
+	float CalculateCrosshairInAirMultiplier(float DeltaTime);
+	float CalculateCrosshairAimingMultiplier(float DeltaTime);
+	float CalculateCrosshairFireAimingMultiplier(float DeltaTime);
+
+	FORCEINLINE float GetCrosshairSpreadMultiplier() const { return CrosshairSpreadMultiplier; }
+	FORCEINLINE float GetCrosshairSpreadMax() const { return CrosshairSpreadMax; };
 };
