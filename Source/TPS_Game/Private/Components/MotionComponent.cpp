90539672876fca34b80156598307af2280aa8834
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-02-02 22:17:44 +0300

    create motion component to control character movement behaviour

---
+++ Source/TPS_Game/Private/Components/MotionComponent.cpp
@@ -0,0 +1,115 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/MotionComponent.h"
+
+#include "Character/ShooterCharacter.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/CombatComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+UMotionComponent::UMotionComponent() :
+	// Turn Rates for aiming/not aiming
+	HipTurnAndLookUpRates(1.f, 1.f),
+	AimingTurnAndLookUpRates(0.5f, 0.5f),
+	RunningSpeed(650.f),
+	CrouchingSpeed(300.f),
+	bIsCrouching(false),
+	RunningHalfHeight(88.f),
+	CrouchingHalfHeight(50.f)
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UMotionComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (OwnerRef == nullptr)
+	{
+		OwnerRef = Cast<AShooterCharacter>(GetOwner());
+	}
+}
+
+void UMotionComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	SetLookRates();
+	UpdateCapsuleHalfHeight(DeltaTime);
+}
+
+void UMotionComponent::Movement(const FVector2D& Value)
+{
+	if (OwnerRef)
+	{
+		/** Movement Direction Vector comes from the keyboard */
+		const FVector2D MovementDirection = Value;
+		/** Controller Rotation which is chanhing by the mouse or etc. */
+		const FRotator ControllerRotation = OwnerRef->GetControlRotation();
+		/** Yaw component of the controller rotation value. */
+		const FRotator YawRotation = FRotator(0.f, ControllerRotation.Yaw, 0.f);
+		/** Set forward vector by using the rotation matrix. */
+		const FVector ForwardVector = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		OwnerRef->AddMovementInput(ForwardVector, MovementDirection.Y);
+		/** Set right vector by using the rotation matrix. */
+		const FVector RightVector = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		OwnerRef->AddMovementInput(RightVector, MovementDirection.X);	
+	}
+}
+
+void UMotionComponent::CrouchStart(const bool& Value)
+{
+	if (OwnerRef)
+	{
+		SetIsCrouching(Value);
+		OwnerRef->GetCharacterMovement()->MaxWalkSpeed = CrouchingSpeed;
+		OwnerRef->GetCharacterMovement()->GroundFriction = 5.f;
+	}
+}
+
+void UMotionComponent::CrouchStop(const bool& Value)
+{
+	if (OwnerRef)
+	{
+		SetIsCrouching(Value);
+		OwnerRef->GetCharacterMovement()->MaxWalkSpeed = RunningSpeed;
+		OwnerRef->GetCharacterMovement()->GroundFriction = .75f;		
+	}
+}
+
+void UMotionComponent::LookAround(const FVector2D& LookDirection)
+{
+	if (OwnerRef)
+	{
+		OwnerRef->AddControllerYawInput(LookDirection.X * BaseTurnAndLoopUpRates.X);
+		OwnerRef->AddControllerPitchInput(LookDirection.Y * BaseTurnAndLoopUpRates.Y);
+	}
+}
+
+void UMotionComponent::SetLookRates()
+{
+	if (OwnerRef && OwnerRef->GetCombatComponent())
+	{
+		OwnerRef->GetCombatComponent()->GetIsAiming() ? BaseTurnAndLoopUpRates = AimingTurnAndLookUpRates : BaseTurnAndLoopUpRates = HipTurnAndLookUpRates;
+	}
+}
+
+void UMotionComponent::UpdateCapsuleHalfHeight(float DeltaTime)
+{
+	bIsCrouching ? TargetHalfHeight = CrouchingHalfHeight : TargetHalfHeight = RunningHalfHeight;
+
+	if (OwnerRef)
+	{
+		const float InterpHalfHeight { FMath::FInterpTo(
+	OwnerRef->GetCapsuleComponent()->GetScaledCapsuleHalfHeight(),
+	TargetHalfHeight,
+	DeltaTime,
+	20.f)};
+
+		const float DeltaCapsuleHalfHeight { InterpHalfHeight - OwnerRef->GetCapsuleComponent()->GetScaledCapsuleHalfHeight() };
+		const FVector MeshOffset { 0.f, 0.f, -DeltaCapsuleHalfHeight};
+		OwnerRef->GetMesh()->AddLocalOffset(MeshOffset);
+	
+		OwnerRef->GetCapsuleComponent()->SetCapsuleHalfHeight(InterpHalfHeight);
+	}
+}
---
+++ Source/TPS_Game/Public/Components/MotionComponent.h
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "MotionComponent.generated.h"
+
+
+class AShooterCharacter;
+struct FInputActionValue;
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class TPS_GAME_API UMotionComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	UMotionComponent();
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+protected:
+	virtual void BeginPlay() override;
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Combat, meta=(AllowPrivateAccess="true"), meta=(ClampMin  = "0.0", ClampMax = "1.0", UIMin = "0.0", UIMax = "1.0"))
+	FVector2D BaseTurnAndLoopUpRates;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Combat, meta=(AllowPrivateAccess="true"), meta=(ClampMin  = "0.0", ClampMax = "1.0", UIMin = "0.0", UIMax = "1.0"))
+	FVector2D HipTurnAndLookUpRates;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Combat, meta=(AllowPrivateAccess="true"), meta=(ClampMin  = "0.0", ClampMax = "1.0", UIMin = "0.0", UIMax = "1.0"))
+	FVector2D AimingTurnAndLookUpRates;
+
+	TObjectPtr<AShooterCharacter> OwnerRef;
+
+	float RunningSpeed;
+	float CrouchingSpeed;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Capsule", meta=(AllowPrivateAccess = "true"))
+	float RunningHalfHeight;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Capsule", meta=(AllowPrivateAccess = "true"))
+	float CrouchingHalfHeight;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Capsule", meta=(AllowPrivateAccess = "true"))
+	float TargetHalfHeight;
+	
+	bool bIsCrouching;
+public:	
+
+	void Movement(const FVector2D& Value);
+	void LookAround(const FVector2D& Value);
+	void CrouchStart(const bool& Value);
+	void CrouchStop(const bool& Value);
+	void SetLookRates();
+	void UpdateCapsuleHalfHeight(float DeltaTime);
+	// Getters
+	FORCEINLINE float GetRunningSpeed() const { return RunningSpeed; }
+	FORCEINLINE float GetCrouchingSpeed() const { return CrouchingSpeed; }
+	FORCEINLINE bool GetIsCrouching() const { return bIsCrouching; }
+	FORCEINLINE void SetIsCrouching(bool bCrouching) { bIsCrouching = bCrouching; }
+};
