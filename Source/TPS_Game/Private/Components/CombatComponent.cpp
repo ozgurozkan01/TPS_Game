b2322295051c9b6d7e8df7d229e364c6941d73b2
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-02-02 22:18:44 +0300

    create combat component to control weapon combat behaviours

--- Source/TPS_Game/Private/Components/CombatComponent.cpp
+++ Source/TPS_Game/Private/Components/CombatComponent.cpp
@@ -1,34 +1,139 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Components/CombatComponent.h"
+#include "Character/ShooterCharacter.h"
+#include "Components/AnimatorComponent.h"
+#include "Components/EffectPlayerComponent.h"
+#include "Components/TracerComponent.h"
+#include "Item/Weapon.h"
+#include "Kismet/GameplayStatics.h"
+#include "Components/MotionComponent.h"
+#include "Particles/ParticleSystemComponent.h"
+#include "GameFramework/Actor.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
-// Sets default values for this component's properties
-UCombatComponent::UCombatComponent()
+UCombatComponent::UCombatComponent() :
+	// Automatic Gun Fire Factors
+	AutomaticFireRate(0.1f),
+	CombatState(ECombatState::ECS_Unoccupied),
+	bAiming(false)
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
-
-
-// Called when the game starts
 void UCombatComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
+	if (OwnerRef == nullptr)
+	{
+		OwnerRef = Cast<AShooterCharacter>(GetOwner());
+	}
 }
-
-
-// Called every frame
 void UCombatComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	// ...
 }
 
+void UCombatComponent::ShootingStart()
+{
+	FVector BeamEndPoint = OwnerRef->GetTracerComponent()->GetBeamEndPoint();
+	FTransform BarrelSocketTransform = OwnerRef->GetEquippedWeapon()->GetBarrelSocketTransform();
+	OwnerRef->GetAnimatorComponent()->PlayGunFireMontage();
+	OwnerRef->GetEffectPlayerComponent()->PlayHitParticle(BeamEndPoint);
+	OwnerRef->GetEffectPlayerComponent()->PlayBeamParticle(BarrelSocketTransform, BeamEndPoint);
+	OwnerRef->GetEffectPlayerComponent()->PlayFireSoundCue();
+	OwnerRef->GetEffectPlayerComponent()->PlayBarrelMuzzleFlash();
+	OwnerRef->GetEquippedWeapon()->DecremenetAmmo();
+	StartFireTimer();
+}
+
+void UCombatComponent::StartAim()
+{
+	SetIsScopeOpen(true);
+	SetIsAiming(true);
+	OwnerRef->GetCharacterMovement()->MaxWalkSpeed = OwnerRef->GetMotionComponent()->GetCrouchingSpeed();
+}
+
+void UCombatComponent::StopAim()
+{
+	SetIsAiming(false);
+	SetIsScopeOpen(false);
+	OwnerRef->GetCharacterMovement()->MaxWalkSpeed = OwnerRef->GetMotionComponent()->GetRunningSpeed();
+}
+
+void UCombatComponent::StartFireTimer()
+{
+	CombatState = ECombatState::ECS_FireTimerInProgress;
+	OwnerRef->GetWorldTimerManager().SetTimer(AutomaticFireHandle, this, &UCombatComponent::AutomaticFireReset, AutomaticFireRate);
+}
+
+void UCombatComponent::AutomaticFireReset()
+{
+	CombatState = ECombatState::ECS_Unoccupied;
+	if (OwnerRef && OwnerRef->GetEquippedWeapon())
+	{
+		if (OwnerRef->GetEquippedWeapon()->GetCurrentAmmo() > 0)
+		{
+			if (OwnerRef->GetIsFireButtonPressed())
+			{
+				StartFireTimer();
+			}	
+		}
+
+		else
+		{
+			ReloadWeapon();
+		}	
+	}
+}
+
+void UCombatComponent::ReloadWeapon()
+{
+	if (CombatState == ECombatState::ECS_Unoccupied && OwnerRef->CarryingAmmo() && !OwnerRef->GetEquippedWeapon()->IsMagazineFull())
+	{
+		if (GetIsAiming())
+		{
+			StopAim();
+		}
+		CombatState = ECombatState::ECS_Reloading;
+		OwnerRef->GetAnimatorComponent()->PlayReloadWeaponMontage();
+	}
+}
+
+
+void UCombatComponent::FinishReloading()
+{
+	CombatState = ECombatState::ECS_Unoccupied;
+
+	if (bIsScopeOpen)
+	{
+		StartAim();
+	}
+
+	if (OwnerRef->GetEquippedWeapon())
+	{
+		const EAmmoType AmmoType = OwnerRef->GetEquippedWeapon()->GetAmmoType();
+
+		if (OwnerRef->GetAmmoMap().Contains(AmmoType))
+		{
+			// Returns to us useable ammo amount of that AmmoType
+			int32 CarriedAmmo = OwnerRef->GetAmmoMap()[AmmoType];
+			// Space left in the magazine of EquippedWeapon
+			const int32 MagazineEmptySpace = OwnerRef->GetEquippedWeapon()->GetMagazineCapacity() - OwnerRef->GetEquippedWeapon()->GetCurrentAmmo();
+
+			if (MagazineEmptySpace > CarriedAmmo)
+			{
+				// Reload the weapon with carried ammo
+				OwnerRef->GetEquippedWeapon()->ReloadAmmo(CarriedAmmo);
+				CarriedAmmo = 0;
+				OwnerRef->UpdateAmmoMap(AmmoType, CarriedAmmo);
+			}
+
+			else
+			{
+				// Fill the weapon
+				OwnerRef->GetEquippedWeapon()->ReloadAmmo(MagazineEmptySpace);
+				CarriedAmmo -= MagazineEmptySpace;
+				OwnerRef->UpdateAmmoMap(AmmoType, CarriedAmmo);
+			}
+		}	
+	}
+}
--- Source/TPS_Game/Public/Components/CombatComponent.h
+++ Source/TPS_Game/Public/Components/CombatComponent.h
@@ -6,23 +6,62 @@
 #include "Components/ActorComponent.h"
 #include "CombatComponent.generated.h"
 
+class AShooterCharacter;
+class UShooterAnimInstance;
+class USoundCue;
+
+UENUM(BlueprintType)
+enum class ECombatState
+{
+	ECS_Unoccupied UMETA(DisplayName = "Unoccupied"),
+	ECS_FireTimerInProgress UMETA(DisplayName = "FireTimerInProgress"),
+	ECS_Reloading UMETA(DisplayName = "Reloading"),
+
+	ECS_MAX UMETA(DisplayName = "Default"),
+};
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class TPS_GAME_API UCombatComponent : public UActorComponent
 {
 	GENERATED_BODY()
-
 public:	
-	// Sets default values for this component's properties
 	UCombatComponent();
-
 protected:
-	// Called when the game starts
 	virtual void BeginPlay() override;
+private:
+	UPROPERTY()
+	TObjectPtr<AShooterCharacter> OwnerRef;
+	
+	/** Automatic Fire Factors*/
+	float AutomaticFireRate;
+	FTimerHandle AutomaticFireHandle;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category=State, meta=(AllowPrivateAccess = "true"))
+	ECombatState CombatState;
+
+	
+	bool bAiming; 
+	bool bIsScopeOpen;
 
 public:	
-	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+	void ShootingStart();
+
+	void StartAim();
+	void StopAim();
+	
+	void StartFireTimer();
+	void AutomaticFireReset();
+
+	void ReloadWeapon(); // This function is for reloading weapon automatically when the magazine is empty 
+	void FinishReloading();
+	
+	FORCEINLINE void SetCombatState(ECombatState CurrentState) { CombatState = CurrentState; }
+	FORCEINLINE void SetIsAiming(bool bIsAiming) { bAiming = bIsAiming; } 
+	FORCEINLINE void SetIsScopeOpen(bool bIsOpen) { bIsScopeOpen = bIsOpen; } 
+	
+	FORCEINLINE ECombatState GetCombatState() const { return CombatState; }
+	FORCEINLINE bool GetIsAiming() const { return bAiming; }
+	FORCEINLINE bool GetIsScopeOpen() const { return bIsScopeOpen; }
 };
