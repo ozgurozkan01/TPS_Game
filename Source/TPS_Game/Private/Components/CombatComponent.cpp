e6761522d5e4f359292be6733dd8aa408b6c932c
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-02-03 23:38:59 +0300

    refactor code by updating the if conditions after inventory class is created

--- Source/TPS_Game/Private/Components/CombatComponent.cpp
+++ Source/TPS_Game/Private/Components/CombatComponent.cpp
@@ -2,6 +2,7 @@
 #include "Character/ShooterCharacter.h"
 #include "Components/AnimatorComponent.h"
 #include "Components/EffectPlayerComponent.h"
+#include "Components/InventoryComponent.h"
 #include "Components/TracerComponent.h"
 #include "Item/Weapon.h"
 #include "Kismet/GameplayStatics.h"
@@ -99,7 +100,10 @@ void UCombatComponent::AutomaticFireReset()
 
 void UCombatComponent::ReloadWeapon()
 {
-	if (CombatState == ECombatState::ECS_Unoccupied && OwnerRef && OwnerRef->CarryingAmmo() && !OwnerRef->GetEquippedWeapon()->IsMagazineFull())
+	if (CombatState == ECombatState::ECS_Unoccupied &&
+		OwnerRef && OwnerRef->GetInventoryComponent() &&
+		OwnerRef->GetInventoryComponent()->CarryingAmmo() &&
+		!OwnerRef->GetEquippedWeapon()->IsMagazineFull())
 	{
 		if (GetIsAiming())
 		{
@@ -120,14 +124,14 @@ void UCombatComponent::FinishReloading()
 		StartAim();
 	}
 
-	if (OwnerRef && OwnerRef->GetEquippedWeapon())
+	if (OwnerRef && OwnerRef->GetInventoryComponent() && OwnerRef->GetEquippedWeapon())
 	{
 		const EAmmoType AmmoType = OwnerRef->GetEquippedWeapon()->GetAmmoType();
 
-		if (OwnerRef->GetAmmoMap().Contains(AmmoType))
+		if (OwnerRef->GetInventoryComponent()->GetAmmoMap().Contains(AmmoType))
 		{
 			// Returns to us useable ammo amount of that AmmoType
-			int32 CarriedAmmo = OwnerRef->GetAmmoMap()[AmmoType];
+			int32 CarriedAmmo = OwnerRef->GetInventoryComponent()->GetAmmoMap()[AmmoType];
 			// Space left in the magazine of EquippedWeapon
 			const int32 MagazineEmptySpace = OwnerRef->GetEquippedWeapon()->GetMagazineCapacity() - OwnerRef->GetEquippedWeapon()->GetCurrentAmmo();
 
@@ -136,7 +140,6 @@ void UCombatComponent::FinishReloading()
 				// Reload the weapon with carried ammo
 				OwnerRef->GetEquippedWeapon()->ReloadAmmo(CarriedAmmo);
 				CarriedAmmo = 0;
-				OwnerRef->UpdateAmmoMap(AmmoType, CarriedAmmo);
 			}
 
 			else
@@ -144,8 +147,9 @@ void UCombatComponent::FinishReloading()
 				// Fill the weapon
 				OwnerRef->GetEquippedWeapon()->ReloadAmmo(MagazineEmptySpace);
 				CarriedAmmo -= MagazineEmptySpace;
-				OwnerRef->UpdateAmmoMap(AmmoType, CarriedAmmo);
 			}
+			
+			OwnerRef->GetInventoryComponent()->UpdateAmmoMap(AmmoType, CarriedAmmo);
 		}	
 	}
 }
