fb6514f344906dc5aa1708f7cbbfcdec8ac36ed8
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-02-09 22:58:12 +0300

    bind widget elements with C++ variables to use control inventory widget

--- Source/TPS_Game/Private/HUD/Item/InventoryWidget.cpp
+++ Source/TPS_Game/Private/HUD/Item/InventoryWidget.cpp
@@ -3,3 +3,47 @@
 
 #include "HUD/Item/InventoryWidget.h"
 
+#include "Character/ShooterCharacter.h"
+#include "HUD/Item/WeaponSlotWidget.h"
+#include "Kismet/GameplayStatics.h"
+
+void UInventoryWidget::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+
+	if (ShooterRef == nullptr)
+	{
+		ShooterRef = Cast<AShooterCharacter>(UGameplayStatics::GetPlayerPawn(this, 0));
+	}	
+	
+	if (CurrentWeaponSlot)
+	{
+		CurrentWeaponSlot->SetSlotIndex(0);
+		InventorySlotArray.Add(CurrentWeaponSlot);
+	}
+	if (WeaponSlot1)
+	{
+		WeaponSlot1->SetSlotIndex(1);
+		InventorySlotArray.Add(WeaponSlot1);
+	}
+	if (WeaponSlot2)
+	{
+		WeaponSlot2->SetSlotIndex(2);
+		InventorySlotArray.Add(WeaponSlot2);
+	}
+	if (WeaponSlot3)
+	{
+		WeaponSlot3->SetSlotIndex(3);
+		InventorySlotArray.Add(WeaponSlot3);
+	}
+	if (WeaponSlot4)
+	{
+		WeaponSlot4->SetSlotIndex(4);
+		InventorySlotArray.Add(WeaponSlot4);
+	}
+	if (WeaponSlot5)
+	{
+		WeaponSlot5->SetSlotIndex(5);
+		InventorySlotArray.Add(WeaponSlot5);
+	}
+}
--- Source/TPS_Game/Public/HUD/Item/InventoryWidget.h
+++ Source/TPS_Game/Public/HUD/Item/InventoryWidget.h
@@ -4,14 +4,35 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
-#include "InventoryWidget.generated.h"
+#include "InventoryWidget.generated.h" 
 
-/**
- * 
- */
+class UWeaponSlotWidget;
+class AShooterCharacter;
+
 UCLASS()
 class TPS_GAME_API UInventoryWidget : public UUserWidget
 {
 	GENERATED_BODY()
-	
+
+public:
+	virtual void NativeOnInitialized() override;
+
+private:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Shooter, meta=(AllowPrivateAccess="true"))
+	TObjectPtr<AShooterCharacter> ShooterRef;
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UWeaponSlotWidget> CurrentWeaponSlot; 
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UWeaponSlotWidget> WeaponSlot1;
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UWeaponSlotWidget> WeaponSlot2;
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UWeaponSlotWidget> WeaponSlot3;
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UWeaponSlotWidget> WeaponSlot4;
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UWeaponSlotWidget> WeaponSlot5;
+
+	TArray<TObjectPtr<UWeaponSlotWidget>> InventorySlotArray;
 };
