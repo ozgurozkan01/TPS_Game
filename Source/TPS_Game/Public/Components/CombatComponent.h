84b93c442c345bc2650a2d1f1e2c855152020a00
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-02-12 18:11:27 +0300

    add equipping variable to the combat state to control weapon equipping behaviours

--- Source/TPS_Game/Public/Components/CombatComponent.h
+++ Source/TPS_Game/Public/Components/CombatComponent.h
@@ -16,6 +16,7 @@ enum class ECombatState
 	ECS_Unoccupied UMETA(DisplayName = "Unoccupied"),
 	ECS_FireTimerInProgress UMETA(DisplayName = "FireTimerInProgress"),
 	ECS_Reloading UMETA(DisplayName = "Reloading"),
+	ECS_Equipping UMETA(DisplayName = "Equipping"),
 
 	ECS_MAX UMETA(DisplayName = "Default"),
 };
