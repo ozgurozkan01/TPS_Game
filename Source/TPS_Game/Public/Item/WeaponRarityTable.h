df84293d958993ef1d02f9cdb9d12f9eb8196d45
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-02-16 19:08:01 +0300

    create weapon rarity table to update glow animation and background image accordingly weapon rarity type

---
+++ Source/TPS_Game/Public/Item/WeaponRarityTable.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "Engine/DataTable.h"
+
+USTRUCT(BlueprintType)
+struct FWeaponRarityTable : FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Table Property")
+	FLinearColor GlowColor;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Table Property")
+	FLinearColor BrightColor;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Table Property")
+	FLinearColor DarkColor;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Table Property")
+	int32 ActiveStar;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Table Property")
+	TObjectPtr<UTexture2D> BackgroundImage;
+};
