7d985e5e949f81602015ef732f47d6954d5b5071
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-02-19 22:57:29 +0300

    add weapon table to control weapon properties accordingly weapon type

---
+++ Source/TPS_Game/Public/Item/WeaponTable.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include "Engine/DataTable.h"
+#include "WeaponTable.generated.h"
+
+USTRUCT(BlueprintType)
+struct FWeaponTable : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Weapon Properties")
+	EAmmoType AmmoType;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Weapon Properties")
+	int32 CurrentAmmoAmount;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Weapon Properties")
+	int32 MazagineCapacity;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Weapon Properties")
+	TObjectPtr<USoundBase> PickupSound;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Weapon Properties")
+	TObjectPtr<USoundBase> EquipSound;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Weapon Properties")
+	TObjectPtr<USkeletalMesh> WeaponMesh;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Weapon Properties")
+	TObjectPtr<UTexture2D> AmmoIcon;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Weapon Properties")
+	TObjectPtr<UTexture2D> WeaponIcon;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Weapon Properties")
+	FString WeaponName;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Weapon Properties")
+	TObjectPtr<UMaterialInstance> MaterialInstance;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Weapon Properties")
+	int32 MaterialIndex;
+};
