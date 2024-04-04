744d0a53837028290d2742f29ad94e5829621737
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-04-04 11:49:45 +0300

    add animation notify class to control character footstep effects

---
+++ Source/TPS_Game/Public/AnimationNotify/FootstepAnimNotify.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "FootstepAnimNotify.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TPS_GAME_API UFootstepAnimNotify : public UAnimNotify
+{
+	GENERATED_BODY()
+};
