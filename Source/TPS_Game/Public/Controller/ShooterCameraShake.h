a4c637a39773a738fa217077c4260b6297da8d80
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-04-07 00:01:45 +0300

    create camera shake class

---
+++ Source/TPS_Game/Public/Controller/ShooterCameraShake.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LegacyCameraShake.h"
+#include "ShooterCameraShake.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TPS_GAME_API UShooterCameraShake : public ULegacyCameraShake
+{
+	GENERATED_BODY()
+	
+};
