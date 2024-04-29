1d7bccb620e2553d598272333ee198abe91b64b6
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-04-30 00:21:50 +0300

    add physicsCore module to be able to return surfaceType

--- Source/TPS_Game/TPS_Game.Build.cs
+++ Source/TPS_Game/TPS_Game.Build.cs
@@ -8,7 +8,7 @@ public class TPS_Game : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG"});
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "PhysicsCore"});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
